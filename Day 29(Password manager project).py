47e62e9a3ae9fa35e8d483ea7fb8691e12128242
Dhanalakshmi lenka <109368262+dhanalakshmilenka256@users.noreply.github.com>
2023-02-27 23:17:00 +0530

    Password Manager program

---
+++ Day 29(Password manager project).py
@@ -0,0 +1,16 @@
+from tkinter import *
+#--------Password Generator-------
+window=Tk()
+window.title("Password Manager")
+
+canvas=Canvas(height=200,weight=200)
+logo_img=PhotoImage(file="C:/Users/Mounika/Desktop/logo.png")
+canvas.create_image(100,100,image=logo_img)
+canvas.pack()
+
+
+
+
+
+
+window.mainloop()
