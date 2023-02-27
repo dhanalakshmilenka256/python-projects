fdf170a333708b1918929fc02bdcfebae0c089a0
Dhanalakshmi lenka <109368262+dhanalakshmilenka256@users.noreply.github.com>
2023-02-28 00:02:48 +0530

    Add files via upload

---
+++ random.py
@@ -0,0 +1,10 @@
+import random
+import my_mod
+random_int=random.randint(1,10)
+print(random_int)
+print(my_mod.pi)
+random_float=random.random()#to display decimal numbers also
+print(random_float)
+score=random.randint(1,100)
+print(f"your score is {score}")
+
